566c70c950d0e48c0c9da886cc318329ab1caf04
Dmitriy Chubatov <dima.don84@gmail.com>
2022-02-10 22:23:57 +0700

    optimization code views.CreateProductView.get, add filter.py

--- cabinet/views.py
+++ cabinet/views.py
@@ -4,6 +4,7 @@ from django.views import View
 
 from .forms import ProductForm, ResponseForm, FilterProductForm
 from .models import Product, Response, Category
+from .filter import Filter
 
 
 class CreateProductView(View):
@@ -91,41 +92,8 @@ class FilterProductView(View):
         form = FilterProductForm(request.GET)
         if form.is_valid():
             cd = form.cleaned_data
-            company = cd["company"]
+            products = Filter.get_products_filter_main(cd)
             category = cd["category"]
-            min_price = cd["min_price"]
-            max_price = cd["max_price"]
-            min_interest_rate = cd["min_interest_rate"]
-            max_interest_rate = cd["max_interest_rate"]
-            period = cd["period"]
-            if not company and not category:
-                products = Product.objects.filter(
-                    price__range=(min_price, max_price),
-                    interest_rate__range=(min_interest_rate, max_interest_rate),
-                    period=period,
-                ).order_by("-date_create")
-            elif company and not category:
-                products = Product.objects.filter(
-                    company=company,
-                    interest_rate__range=(min_interest_rate, max_interest_rate),
-                    price__range=(min_price, max_price),
-                    period=period,
-                ).order_by("-date_create")
-            elif company and category:
-                products = Product.objects.filter(
-                    company=company,
-                    category=category,
-                    price__range=(min_price, max_price),
-                    interest_rate__range=(min_interest_rate, max_interest_rate),
-                    period=period,
-                ).order_by("-date_create")
-            elif not company and category:
-                products = Product.objects.filter(
-                    category=category,
-                    price__range=(min_price, max_price),
-                    interest_rate__range=(min_interest_rate, max_interest_rate),
-                    period=period,
-                ).order_by("-date_create")
             categories = Category.objects.all()
             response_form = ResponseForm()
             form_filter = FilterProductForm(request.GET)
